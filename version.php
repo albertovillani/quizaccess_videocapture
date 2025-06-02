2dde89cf5b229d3ad00dc185e0df50bc68c63a23
Ubuntu <ubuntu@ip-172-31-34-74.eu-central-1.compute.internal>
2025-06-02 09:49:48 +0000

    Changed version for Safari support

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2025010700;
+$plugin->version   = 2025060200;
 $plugin->requires  = 2024100700;
 $plugin->component = 'quizaccess_videocapture';
